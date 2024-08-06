1472aa5cb0770fc167db399ee69699da6fda37f9
BigMinionD <littleminiond@outlook.com>
2024-08-05 21:57:39 -0500

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
+	name: "The Multiversal Tree",
+	id: "multiverse",
+	author: "BigMinionD",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
 
